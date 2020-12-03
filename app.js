1bede7637cd385fcb97e8e69ab26f99f2e3c49ee
Paul Stevens <twinkisan@hotmail.com>
2020-12-03 14:48:03 -0800

    working server

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/test/', (req, res) => {
+    console.log(req.body);
+    res.status(200).json({ greeting: 'HELLO POSTMAN!' })
+})
+
 app.listen(3000, () => {
     console.log('HI! 3000')
 })
